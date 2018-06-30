c45d093c686f55cc217c4ee17029b23cd89ffdeb
EugeneBogush <eugeneb2008@gmail.com>
2018-06-30 18:12:11 +0300

    Update cwmp.go
    Add CWMP:ID

--- cwmp/cwmp.go
+++ cwmp/cwmp.go
@@ -147,10 +147,14 @@ func InformResponse(mustUnderstand string) string {
 </soap:Envelope>`
 }
 
-func GetParameterValues(leaf string) string {
+func GetParameterValues(mustUnderstand string, leaf string) string {
+	mustUnderstandHeader := ""
+	if mustUnderstand != "" {
+		mustUnderstandHeader = `<cwmp:ID soap:mustUnderstand="1">` + mustUnderstand + `</cwmp:ID>`
+	}
 	return `<?xml version="1.0" encoding="UTF-8"?>
 <soap:Envelope xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:cwmp="urn:dslforum-org:cwmp-1-0" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:schemaLocation="urn:dslforum-org:cwmp-1-0 ..\schemas\wt121.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <soap:Header/>
+  <soap:Header>` + mustUnderstandHeader + `</soap:Header>
   <soap:Body soap:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
     <cwmp:GetParameterValues>
       <ParameterNames>
